41897c73bb5c7f6266bad95deb9c53165346c7dd
KELVIN FERNANDES SILVA <kelvin.fernandess04@hotmail.com>
2024-05-23 20:52:08 -0300

    Sobre pronto

---
+++ PBL-MVC/Views/Home/Sobre.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PBL_MVC.Views.Home
+{
+    public class SobreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
